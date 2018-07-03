fd8501bde677112457d6ee6a0336a38020e25a32
Andrew Driban <dribaan@habasitusa.net>
2018-07-03 14:09:20 -0400

    got alarm working, and can get widget to display, but for some reason the track won't play, no matter which methods I use

--- clockApp.js
+++ clockApp.js
@@ -18,39 +18,18 @@ var clockApp = {
         e.preventDefault();
         var hours = $("#hoursInput").val();
         var minutes = $("#minutesInput").val();
-        var clock = hours + ":" + minutes;
-
-        SC.initialize({
-            client_id: 'Andrew Driban',
-            // redirect_uri: 'http://example.com/callback'
-        });
-        
-        var track_url = 'https://soundcloud.com/olivertree/sets/alien-boy-ep-1';
-        SC.oEmbed(track_url, { auto_play: true }).then(function(oEmbed) {
-        console.log('oEmbed response: ', oEmbed);
-        if (moment().format("H:mm") == clock) {
-            $("#music").append(oEmbed.html);
-        }
-        });
+        var clock = hours + ":" + minutes;        
+        var alarm = setInterval(function() {
+            if (moment().format("H:mm") == clock) {
+                var widget = SC.Widget("widg");
+                widget.play();
+                clearInterval(alarm);
+            }
+        }, 1000);
     }
 }
+
 
 $(document).ready(function() {clockApp.dropDownSet()})
 
 $("#clockSet").on("click", function(e) {clockApp.clockSet(e)})
-
-{/* <script src="https://connect.soundcloud.com/sdk/sdk-3.3.0.js"></script>
-<script>
-SC.initialize({
-  client_id: 'YOUR_CLIENT_ID'
-});
-
-// stream track id 293
-SC.stream('/tracks/293').then(function(player){
-  player.play().then(function(){
-    console.log('Playback started!');
-  }).catch(function(e){
-    console.error('Playback rejected. Try calling play() from a user interaction.', e);
-  });
-});
-</script> */}
