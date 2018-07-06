df895dd9b0fafe561d460c64bc176f65626b58ff
Andrew <andrewmd4774@gmail.com>
2018-07-05 21:31:22 -0400

    starting spotify

--- clockApp.js
+++ clockApp.js
@@ -19,48 +19,44 @@ var clockApp = {
         var hours = $("#hoursInput").val();
         var minutes = $("#minutesInput").val();
         var clock = hours + ":" + minutes;  
-        var q = $("#soch").val().trim();
+
+        function generateSpotifyAccessToken(cb) {
+            $.ajax({
+                url: 'https://cors-anywhere.herokuapp.com/https://accounts.spotify.com/api/token',
+                method: "POST",
+                data: {
+                    grant_type: "client_credentials"
+                },
+                headers: {
+                    Authorization: "Basic " + btoa("21dad2969f2947419ba9fa2cac7560d8" + ":" + "5d62cecbdad943d9b8c5047a6c9e8aac")
+                }
+            }).then(res => {
+                spotify_access_token = res.access_token;
+                cb();
+            }).catch(err => console.error(err));
+        };
         
-        // // find all sounds of buskers licensed under 'creative commons share alike'
-        // SC.connect().then(function(){
-        //     SC.get('/tracks', {
-        //         q: q }).then(function(tracks) {
-        //         console.log(tracks);
-        //     });
-        // });
+        //         //
+        // Spotify //
+        //         //
+        function getArtist(artist, cb) {
+            $.ajax({
+                method: 'GET',
+                url: 'https://api.spotify.com/v1/search',
+                data: {
+                    q: artist,
+                    type: 'track'
+                },
+                headers: {
+                    Authorization: "Bearer " + spotify_access_token
+                }
+            }).then(cb).catch(() => generateSpotifyAccessToken(() => getArtist(artist, cb)));
+        }
         
         var alarm = setInterval(function() {
             if (moment().format("H:mm") == clock) {
-
-                const play = ({
-                    spotify_uri,
-                    playerInstance: {
-                      _options: {
-                        getOAuthToken,
-                        id
-                      }
-                    }
-                  }) => {
-                    getOAuthToken(access_token => {
-                      fetch(`https://api.spotify.com/v1/me/player/play?device_id=${id}`, {
-                        method: 'PUT',
-                        body: JSON.stringify({ uris: [spotify_uri] }),
-                        headers: {
-                          'Content-Type': 'application/json',
-                          'Authorization': `Bearer ${access_token}`
-                        },
-                      });
-                    });
-                  };
-                  
-                  play({
-                    playerInstance: new Spotify.Player({ name: "..." }),
-                    spotify_uri: 'spotify:track:7xGfFoTpQ2E7fRF5lN10tr',
-                  });
-
-                // var widget = SC.Widget("widg");
-                // widget.play();
-                // clearInterval(alarm);
+                console.log("wake up idiot");
+                clearInterval(alarm);
             }
         }, 1000);
     },
@@ -73,6 +69,7 @@ var clockApp = {
         e.preventDefault();
         url = MQurl + $.param({key: MQapikey, from: $("#yolo").val(), to: $("#dest").val()});
         $.ajax(url, "Get").then(function(response) {
+            console.log(response);
             var man = response.route.legs[0].maneuvers;
             for (var i in man) {
                 $(dir).append(response.route.legs[0].maneuvers[i].narrative + "<br>");
